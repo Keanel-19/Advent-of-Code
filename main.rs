dfd199bfbae2333a2793086b108561992624fc34
Mael Saillot <mael.saillot@trlecomnancy.eu>
2023-12-07 01:52:46 +0100

    D02P1

--- main.rs
+++ main.rs
@@ -1,9 +1,6 @@
 use std::fs::read_to_string;
+use regex::Regex;
 
-struct Digit {
-    index: usize,
-    value: u32
-}
 
 fn read_input() -> Vec<String> {
     read_to_string("input")
@@ -13,55 +10,24 @@ fn read_input() -> Vec<String> {
        .collect()
 }
 
-fn find_first(line: &String, pat: &str, value: u32, old: Option<Digit>) -> Option<Digit> {
-    let index = line.find(pat);
-    match (index,old) {
-        (Some(i), Some(d)) => if i < d.index {Some(Digit{index:i,value})} else {Some(d)},
-        (None, old) => old,
-        (Some(i), None) => Some(Digit{index:i,value})
-    }
-}
-
-fn find_last(line: &String, pat: &str, value: u32, old: Option<Digit>) -> Option<Digit> {
-    let index = line.rfind(pat);
-    match (index,old) {
-        (Some(i), Some(d)) => if i > d.index {Some(Digit{index:i,value})} else {Some(d)},
-        (None, old) => old,
-        (Some(i), None) => Some(Digit{index:i,value})
-    }
-}
-
 fn main(){
-    let mut total = 0;
-    let digits = [
-        ("one",1),
-        ("1",1),
-        ("two",2),
-        ("2",2),
-        ("three",3),
-        ("3",3),
-        ("four",4),
-        ("4",4),
-        ("five",5),
-        ("5",5),
-        ("six",6),
-        ("6",6),
-        ("seven",7),
-        ("7",7),
-        ("eight",8),
-        ("8",8),
-        ("nine",9),
-        ("9",9),
-    ];
+let mut total = 0;
+    let reg = Regex::new(r"Game (\d+):").unwrap();
+    let reg2 = Regex::new(r"(\d+) (red|green|blue)").unwrap();
     
     for line in read_input().iter() {
-        let mut first = None;
-        let mut last = None;
-        for t in digits.iter() {
-            first = find_first(line, t.0, t.1, first);
-            last = find_last(line, t.0, t.1, last);
+        let caps = reg.captures(line).unwrap();
+        let game: u32 = caps.get(1).unwrap().as_str().parse().unwrap();
+        
+        if reg2.captures_iter(line)
+        .all(|c| match (c.get(2).unwrap().as_str(), c.get(1).unwrap().as_str().parse::<u32>().unwrap()) {
+            ("red",v) => v<=12,
+            ("green",v) => v<=13,
+            ("blue",v) => v<=14,
+            _ => true
+        }) {
+            total += game
         }
-        total += first.unwrap().value*10 + last.unwrap().value
     }
-    println!("{}", total)
+    println!("{}",total)
 }
