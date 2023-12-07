d3ac3b994f20d5cbf0ba0e24d906e48b8b1e4117
Mael Saillot <mael.saillot@trlecomnancy.eu>
2023-12-07 08:14:40 +0100

    D04P2

--- main.rs
+++ main.rs
@@ -11,43 +11,24 @@ fn read_input() -> Vec<String> {
 }
 
 fn main(){
-let mut total =0;
-    let reg = Regex::new(r"\d+").unwrap();
-    let reg2 = Regex::new(r"\*").unwrap();
+    let mut total =0;
+    let reg = Regex::new(r"^Card +\d+:([ \d]+)\|([ \d]+)$").unwrap();
+    let reg2 = Regex::new(r"\d+").unwrap();
     
-    let plan = read_input();
-    let mut syms = vec![vec![(-1,0,0); plan[0].len()]; plan.len()];
+    let cards = read_input();
+    let mut quantity = vec![1; cards.len()];
     
-    for i in 0..plan.len() {
-        for j in reg2.find_iter(&plan[i]).map(|m| m.start()) {
-            syms[i][j].0 = 0
+    for i in 0..cards.len() {
+        let line = &cards[i];
+        let caps = reg.captures(line).unwrap();
+        let targets : Vec<u32> = reg2.find_iter(&caps[1]).map(|s| s.as_str().parse().unwrap()).collect();
+        let winning_count = reg2.find_iter(&caps[2]).map(|s| s.as_str().parse::<u32>().unwrap()).filter(|n| targets.contains(n)).count();
+        for j in 0..winning_count {
+            quantity[i+1+j] += quantity[i]
         }
     }
     
-    for i in 0..plan.len() {
-        for m in reg.find_iter(&plan[i]) {
-            let n : u32 = m.as_str().parse().unwrap();
-            for k in if i==0 {0} else {i-1}..=if i+1>=syms.len() {i} else {i+1} {
-                for l in if m.start()==0 {0} else {m.start()-1}..if m.end()+1>=syms.len() {m.end()} else {m.end()+1}{
-                match syms[k][l].0 {
-                    -1 => (),
-                    0 => {syms[k][l].0 = 1; syms[k][l].1= n},
-                    1 => {syms[k][l].0 = 2; syms[k][l].2 = n},
-                    _ => syms[k][l].0 = -1
-                }
-                }
-            }
-        }
-    }
-    
-    for l in syms.iter() {
-        for t in l.iter() {
-            if t.0 == 2 {
-                total += t.1*t.2
-            }
-        }
-    }
-    
+    total = quantity.iter().sum();
     println!("{}", total)
     
 }
