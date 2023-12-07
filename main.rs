65af7f1aefb08f6901d7768147bb41c3e1104722
Mael Saillot <mael.saillot@trlecomnancy.eu>
2023-12-07 03:18:25 +0100

    D03P2

--- main.rs
+++ main.rs
@@ -11,24 +11,33 @@ fn read_input() -> Vec<String> {
 }
 
 fn main(){
-let mut total = 0;
-    let reg = Regex::new(r"Game (\d+):").unwrap();
-    let reg2 = Regex::new(r"(\d+) (red|green|blue)").unwrap();
+let mut total =0;
+    let reg = Regex::new(r"\d+").unwrap();
+    let reg2 = Regex::new(r"[^.\d]").unwrap();
     
-    for line in read_input().iter() {
-        let caps = reg.captures(line).unwrap();
-        let game: u32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        
-        let mut trip = (0,0,0);
-        
-        for c in reg2.captures_iter(line) {
-            match (c.get(2).unwrap().as_str(), c.get(1).unwrap().as_str().parse::<u32>().unwrap()) {
-            ("red",v) => if (v > trip.0) {trip.0 = v},
-            ("green",v) => if (v > trip.1) {trip.1 = v},
-            ("blue",v) => if (v > trip.2) {trip.2 = v},
-            _ => ()
-        }}
-        total += trip.0*trip.1*trip.2
+    let plan = read_input();
+    let mut syms = vec![vec![false; plan[0].len()]; plan.len()];
+    
+    for i in 0..plan.len() {
+        for j in reg2.find_iter(&plan[i]).map(|m| m.start()) {
+            syms[i][j] = true
+        }
     }
-    println!("{}",total)
+    
+    for i in 0..plan.len() {
+        for m in reg.find_iter(&plan[i]) {
+            let mut n : u32 = m.as_str().parse().unwrap();
+            for k in if i==0 {0} else {i-1}..=if i+1>=syms.len() {i} else {i+1} {
+                for l in if m.start()==0 {0} else {m.start()-1}..if m.end()+1>=syms.len() {m.end()} else {m.end()+1}{
+                    if syms[k][l] {
+                        total += n;
+                        n = 0;
+                    }
+                }
+            }
+        }
+    }
+    
+    println!("{}", total)
+    
 }
