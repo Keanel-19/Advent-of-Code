2e59105db5f61643d257e086aa470ff849a732e8
Mael Saillot <mael.saillot@trlecomnancy.eu>
2023-12-07 12:10:18 +0100

    D05P2

--- main.rs
+++ main.rs
@@ -8,6 +8,12 @@ struct Rel {
     len: u32
 }
 
+#[derive(Debug)]
+struct Seeds {
+    start: u32,
+    len: u32
+}
+
 fn read_input() -> Vec<String> {
     read_to_string("input")
        .unwrap()
@@ -25,9 +31,9 @@ fn main(){
     
     //println!("{:?}", i_maps);
     
-    let reg = Regex::new(r"\d+").unwrap();
-    let mut seeds : Vec<u32>= reg.find_iter(&text[0]).map(|m| m.as_str().parse().unwrap()).collect();
-    seeds.sort();
+    let reg = Regex::new(r"(\d+) (\d+)").unwrap();
+    let mut seeds : Vec<Seeds>= reg.captures_iter(&text[0]).map(|c| Seeds{start:c[1].parse().unwrap(), len:c[2].parse().unwrap()}).collect();
+    seeds.sort_by(|s1,s2| s1.start.cmp(&s2.start));
     
     //println!("{:?}", seeds);
     
@@ -48,26 +54,72 @@ fn main(){
         
         let mut iseed = 0;
         let mut irel = 0;
+        let mut newseed: Vec<Seeds> = Vec::new();
         
         while iseed < seeds.len() && irel < map.len() {
-            let s = seeds[iseed];
+            let s = &seeds[iseed];
             let r = &map[irel];
+            println!("{:?} {:?}",s,r );
             
-            if s < r.from {
-                iseed+=1;
-            } else if s-r.from >= r.len {
+            if s.start < r.from {
+                println!("cat1");
+                if s.start+s.len-1 < r.from {
+                    newseed.push(Seeds{
+                        start:s.start,
+                        len:s.len
+                    });
+                    iseed+=1
+                } else {
+                    newseed.push(Seeds{
+                        start:s.start,
+                        len: r.from - s.start
+                    });
+                    seeds[iseed] = Seeds{
+                        start:r.from,
+                        len: s.start + s.len - r.from 
+                    };
+                }
+            } else if s.start-r.from >= r.len {
                 irel += 1;
+                println!("cat2");
             } else {
-                seeds[iseed] = s-r.from+r.to;
-                iseed +=1;
+                println!("cat3");
+                if s.start-r.from+s.len > r.len {
+                println!("scat1");
+                    newseed.push(Seeds{
+                        start: s.start-r.from+r.to,
+                        len: r.from + r.len - s.start
+                    });
+                    seeds[iseed] = Seeds{
+                        start:r.from+r.len,
+                        len:s.start-r.from+s.len-r.len
+                    }
+                } else {
+                    println!("scat2");
+                    newseed.push(Seeds{
+                        start: s.start-r.from+r.to,
+                        len: s.len
+                    });
+                    iseed+=1;
+                }
             }
         }
-        seeds.sort();
+        if irel >= map.len() {
+            for u in iseed..seeds.len() {
+                let s = &seeds[u];
+                newseed.push(Seeds{
+                    start:s.start,
+                    len:s.len
+                })
+            }
+        }
+        seeds = newseed;
+        seeds.sort_by(|s1,s2| s1.start.cmp(&s2.start));
         
-        //println!("{:?}", seeds);
+        println!("{:?}", seeds);
     }
     //println!("{:?}", seeds);
-    let total = seeds[0];
+    let total = seeds[0].start;
     println!("{}", total)
     
 }
