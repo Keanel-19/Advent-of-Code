1315cce0199901381141d7dbe91d6746806c8196
Mael Saillot <mael.saillot@trlecomnancy.eu>
2023-12-11 10:33:02 +0100

    D11P2

--- main.rs
+++ main.rs
@@ -14,114 +14,50 @@ fn main(){
     let text = read_input();
     //let reg = Regex::new(r"-?\d+").unwrap();
     
-    let mut grid: Vec<Vec<char>> = text.iter().map(|s| s.chars().collect()).collect();
+    let grid: Vec<Vec<char>> = text.iter().map(|s| s.chars().collect()).collect();
     
-    let s_point = (0..grid.len()).flat_map(|i| (0..grid[i].len()).map(move |j| (i,j))).filter(|t| grid[t.0][t.1]=='S').next().unwrap();
+    let erows: Vec<usize> = (0..grid.len()).filter(|i| (0..grid[*i].len()).all(|j| grid[*i][j]=='.')).collect();
+    let ecols: Vec<usize> = (0..grid[0].len()).filter(|j| (0..grid.len()).all(|i| grid[i][*j]=='.')).collect();
     
-    let starts = [
-        if 0 < s_point.0 {Some((s_point.0-1,s_point.1))} else {None},
-        if 0 < s_point.1 {Some((s_point.0,s_point.1-1))} else {None},
-        if s_point.0+1 < grid.len() {Some((s_point.0+1,s_point.1))} else {None},
-        if s_point.1+1 < grid[s_point.0].len() {Some((s_point.0,s_point.1+1))} else {None}
-    ].into_iter().filter(|x| x.is_some()).map(|x| x.unwrap());
-    
-    let mut path = vec![];
-    
-    for p in starts {
-        let mut actuel = s_point;
-        let mut next = p;
-        path = vec![p];
-        let mut found = false;
-        
-        loop {
-            if grid[next.0][next.1] == 'S' {
-                found = true;
-                break
-            }
-            (actuel,next) = (next,match (next.0 as isize-actuel.0 as isize,next.1 as isize-actuel.1 as isize) {
-                ( 1,0) => match grid[next.0][next.1] {
-                    '|' => (next.0+1,next.1),
-                    'L' => (next.0,next.1+1),
-                    'J' => (next.0,next.1-1),
-                    _ => break
-                },
-                (-1,0) => match grid[next.0][next.1] {
-                    '|' => (next.0-1,next.1),
-                    'F' => (next.0,next.1+1),
-                    '7' => (next.0,next.1-1),
-                    _ => break
-                },
-                (0, 1) => match grid[next.0][next.1] {
-                    '-' => (next.0,next.1+1),
-                    '7' => (next.0+1,next.1),
-                    'J' => (next.0-1,next.1),
-                    _ => break
-                },
-                (0,-1) => match grid[next.0][next.1] {
-                    '-' => (next.0,next.1-1),
-                    'L' => (next.0-1,next.1),
-                    'F' => (next.0+1,next.1),
-                    _ => break
-                },
-                _ => break
-            });
-            path.push(next);
-        }
-        if found {
-            break
-        }
-    }
-    
-    let mut g = vec![vec![false;grid[0].len()];grid.len()];
-    for (i,j) in path.iter() {
-        g[*i][*j] = true
-    }
-    
-    let a = path[0];
-    let b = path[path.len()-2];
-    grid[s_point.0][s_point.1] = match (
-        s_point.0 as isize-a.0 as isize,
-        s_point.1 as isize-a.1 as isize,
-        s_point.0 as isize-b.0 as isize,
-        s_point.1 as isize-b.1 as isize
-    ) {
-        (1,0,-1,0) => '|',
-        (-1,0,1,0) => '|',
-        (0,1,0,-1) => '-',
-        (0,-1,0,1) => '-',
-        (0,1,1,0) => 'J',
-        (1,0,0,1) => 'J',
-        (0,-1,-1,0) => 'F',
-        (-1,0,0,-1) => 'F',
-        (0,-1,1,0) => 'L',
-        (1,0,0,-1) => 'L',
-        (0,1,-1,0) => '7',
-        (-1,0,0,1) => '7',
-        _ => panic!()
-    };
-    
-    
-    for i in 0..g.len() {
-        let mut inner = false;
-        let mut o = 0;
-        for j in 0..g[i].len() {
-            if g[i][j] {
-                o += match grid[i][j] {
-                    'L'|'J' => 1,
-                    '7'|'F' => -1,
-                    _ => 0
-                };
-                if o == 0 {
-                    inner = !inner
-                } else if o == 2 || o == -2 {
-                    o = 0
-                }
-                
-            } else if inner {
-                total+=1
+    let mut stars = vec![];
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            if grid[i][j]=='#' {
+                stars.push((i,j))
             }
         }
     }
     
-    println!("{:?}", total)
+    
+    for s in 0..stars.len()-1 {
+        for t in s..stars.len() {
+            let (x,y) = stars[s];
+            let (u,v) = stars[t];
+            total += if x > u {
+                x-u
+            } else {
+                u-x
+            };
+            
+            total += if x > u {
+                erows.iter().filter(|i| u<**i && **i<x).count()
+            } else {
+                erows.iter().filter(|i| x<**i && **i<u).count()
+            }*(1000000-1);
+            
+            total += if y > v {
+                y-v
+            } else {
+                v-y
+            };
+            
+            total += if y > v {
+                ecols.iter().filter(|j| v<**j && **j<y).count()
+            } else {
+                ecols.iter().filter(|j| y<**j && **j<v).count()
+            }*(1000000 -1);
+        }
+    }
+    
+    println!("{}",total)
 }
