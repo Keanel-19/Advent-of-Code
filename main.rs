5a115301ffe4aa7fd766e07d48ac5b019f5c5f32
Mael Saillot <mael.saillot@trlecomnancy.eu>
2023-12-13 09:51:28 +0100

    D13P1

--- main.rs
+++ main.rs
@@ -1,5 +1,5 @@
 use std::fs::read_to_string;
-use regex::Regex;
+//use regex::Regex;
 
 fn read_input() -> Vec<String> {
     read_to_string("input")
@@ -9,89 +9,50 @@ fn read_input() -> Vec<String> {
        .collect()
 }
 
-#[derive(PartialEq,Clone,Copy,Debug)]
-enum Spring {
-    Good,
-    Bad,
-    Both
-}
-use Spring::{Good,Bad,Both};
-
-fn calculate(spr: &Vec<Spring>, pat: &Vec<usize>) -> u64 {
-    let mut mem = vec![vec![None;pat.len()];spr.len()];
-    
-    fn can_pose(spr: &Vec<Spring>, from: usize, l: usize) -> bool {
-        for i in from..from+l {
-            if !(i < spr.len() && spr[i]!=Good) {
-                return false
-            }
-        }
-        if from+l < spr.len() && spr[from+l]==Bad {
-            return false
-        }
-        true
-    }
-    
-    fn walk(mem: &mut Vec<Vec<Option<u64>>> ,spr: &Vec<Spring>, pat: &Vec<usize>, i: usize, j: usize) -> u64 {
-        if i < spr.len() && j < pat.len() {
-            let m = mem[i][j];
-            if m.is_some() {
-                return m.unwrap()
-            }
-            let m = match spr[i] {
-                Good => walk(mem,spr,pat,i+1,j),
-                Bad => if can_pose(spr,i,pat[j]) {
-                    walk(mem,spr,pat,i+pat[j]+1,j+1)
-                } else {
-                    0
-                },
-                Both => (if can_pose(spr,i,pat[j]) {
-                    walk(mem,spr,pat,i+pat[j]+1,j+1)
-                } else {
-                    0
-                }) + walk(mem,spr,pat,i+1,j)
-            };
-            mem[i][j] = Some(m);
-            m
-        } else if j >= pat.len() {
-            for a in i..spr.len() {
-                if spr[a] == Bad {
-                    return 0
-                }
-            }
-            1
-        } else {
-            0
+fn find_ref(v: &Vec<u64>) -> usize {
+    for c in 1..v.len() {
+        let len = if c < v.len()-c {c} else {v.len()-c};
+        if (0..len).all(|i| v[c-1-i]==v[c+i]) {
+            return c
         }
     }
-    
-    walk(&mut mem,&spr,&pat,0,0)
+    0
 }
-
-//format!("{0}#{0}#{0}#{0}#{0}",&cap[1])
 
 fn main(){
     let mut total = 0;
     let text = read_input();
-    let reg = Regex::new(r"([.#?]+) ([\d,]+)").unwrap();
+    //let reg = Regex::new(r"([.#?]+) ([\d,]+)").unwrap();
     
-    for line in text.iter() {
-        let cap = reg.captures(line).unwrap();
+    let mut maps: Vec<Vec<String>> = vec![vec![]];
+    for line in text.into_iter() {
+        if line.len() > 0 {
+            let last = maps.len()-1;
+            maps[last].push(line)
+        } else {
+            maps.push(vec![])
+        }
+    }
+    
+    for grid in maps.into_iter() {
+        let mut rows: Vec<u64> = vec![0;grid.len()];
+        let mut cols: Vec<u64> = vec![0;grid[0].len()];
         
-        let init_states: Vec<Spring> = format!("{0}?{0}?{0}?{0}?{0}",&cap[1]).chars().map(|c| match c {
-            '.' => Good,
-            '#' => Bad,
-            '?' => Both,
-            _ => panic!()
-        }).collect();
-        //println!("{:?}",init_states);
+        for i in 0..grid.len() {
+            for j in 0..grid[i].len() {
+                let digit = match grid[i].chars().nth(j) {
+                    Some('.') => 0,
+                    Some('#') => 1,
+                    _ => panic!()
+                };
+                rows[i] = (rows[i] << 1) + digit;
+                cols[j] = (cols[j] << 1) + digit;
+            }
+        }
         
-        let groups: Vec<usize> = format!("{0},{0},{0},{0},{0}",&cap[2]).split(',').map(|s| s.parse().unwrap()).collect();
-        //println!("{:?}",groups);
+        total += find_ref(&cols) + 100*find_ref(&rows);
         
-        let subtot = calculate(&init_states,&groups);
-        total+=subtot;
-        //println!("{}",subtot)
+        //println!("{:?} {:?}",rows,cols)
     }
     
     println!("{}",total)
