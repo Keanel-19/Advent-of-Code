6808131a510e77c0a9e1af12c6f2e5bdb28a4480
Mael Saillot <mael.saillot@trlecomnancy.eu>
2023-12-19 13:51:54 +0100

    D19P2

--- main.rs
+++ main.rs
@@ -25,6 +25,67 @@ impl Gear {
     }
 }
 
+
+
+#[derive(Clone,Copy,Debug)]
+struct GearRange {
+    x: (u32,u32),
+    m: (u32,u32),
+    a: (u32,u32),
+    s: (u32,u32)
+}
+
+impl GearRange {
+    fn new(t:(u32,u32)) -> GearRange {
+        GearRange {
+            x:t,
+            m:t,
+            a:t,
+            s:t
+        }
+    }
+
+    fn count(&self) -> u64 {
+        fn len(t:(u32,u32)) -> u64 {
+            (t.1-t.0+1) as u64
+        }
+        len(self.x)*len(self.m)*len(self.a)*len(self.s)
+    }
+    
+    fn split_x(&self,l:u32) -> (GearRange,GearRange) {
+        let mut inf = self.clone();
+        let mut sup = self.clone();
+        inf.x.1 = l;
+        sup.x.0 = l+1;
+        (inf,sup)
+    }
+    
+    fn split_m(&self,l:u32) -> (GearRange,GearRange) {
+        let mut inf = self.clone();
+        let mut sup = self.clone();
+        inf.m.1 = l;
+        sup.m.0 = l+1;
+        (inf,sup)
+    }
+    
+    fn split_a(&self,l:u32) -> (GearRange,GearRange) {
+        let mut inf = self.clone();
+        let mut sup = self.clone();
+        inf.a.1 = l;
+        sup.a.0 = l+1;
+        (inf,sup)
+    }
+    
+    fn split_s(&self,l:u32) -> (GearRange,GearRange) {
+        let mut inf = self.clone();
+        let mut sup = self.clone();
+        inf.s.1 = l;
+        sup.s.0 = l+1;
+        (inf,sup)
+    }
+}
+
+
 #[derive(Clone,Debug)]
 enum Rule {
     Sup(String,u32),
@@ -32,6 +93,35 @@ enum Rule {
     Always
 }
 use Rule::{Sup,Inf,Always};
+
+struct RuleRes<T> {
+    accepted: Option<T>,
+    rejected: Option<T>
+}
+
+
+impl<T> RuleRes<T> {
+    fn new(a:T,r:T) -> RuleRes<T>{
+        RuleRes {
+            accepted: Some(a),
+            rejected: Some(r)
+        }
+    }
+
+    fn a(v:T) -> RuleRes<T> {
+        RuleRes {
+            accepted: Some(v),
+            rejected: None
+        }
+    }
+    
+    fn r(v:T) -> RuleRes<T> {
+        RuleRes {
+            accepted: None,
+            rejected: Some(v)
+        }
+    }
+}
 
 
 impl Rule {
@@ -54,6 +144,83 @@ impl Rule {
             Always => true
         }
     }
+    
+    
+    fn test_range(&self,g:GearRange) -> RuleRes<GearRange> {
+        match self {
+            Sup(s,v) => match s.as_str() {
+                "x" => if g.x.0>*v {
+                    RuleRes::a(g)
+                } else if g.x.1 >*v {
+                    let (r,a) = g.split_x(*v);
+                    RuleRes::new(a,r)
+                } else {
+                    RuleRes::r(g)
+                },
+                "m" => if g.m.0>*v {
+                    RuleRes::a(g)
+                } else if g.m.1 >*v {
+                    let (r,a) = g.split_m(*v);
+                    RuleRes::new(a,r)
+                } else {
+                    RuleRes::r(g)
+                },
+                "a" => if g.a.0>*v {
+                    RuleRes::a(g)
+                } else if g.a.1 >*v {
+                    let (r,a) = g.split_a(*v);
+                    RuleRes::new(a,r)
+                } else {
+                    RuleRes::r(g)
+                },
+                "s" => if g.s.0>*v {
+                    RuleRes::a(g)
+                } else if g.s.1 >*v {
+                    let (r,a) = g.split_s(*v);
+                    RuleRes::new(a,r)
+                } else {
+                    RuleRes::r(g)
+                },
+                _ => panic!()
+            },
+            Inf(s,v) => match s.as_str() {
+                "x" => if g.x.1<*v {
+                    RuleRes::a(g)
+                } else if g.x.0 <*v {
+                    let (a,r) = g.split_x(*v-1);
+                    RuleRes::new(a,r)
+                } else {
+                    RuleRes::r(g)
+                },
+                "m" => if g.m.1<*v {
+                    RuleRes::a(g)
+                } else if g.m.0 <*v {
+                    let (a,r) = g.split_m(*v-1);
+                    RuleRes::new(a,r)
+                } else {
+                    RuleRes::r(g)
+                },
+                "a" => if g.a.1<*v {
+                    RuleRes::a(g)
+                } else if g.a.0 <*v {
+                    let (a,r) = g.split_a(*v-1);
+                    RuleRes::new(a,r)
+                } else {
+                    RuleRes::r(g)
+                },
+                "s" => if g.s.1<*v {
+                    RuleRes::a(g)
+                } else if g.s.0 <*v {
+                    let (a,r) = g.split_s(*v-1);
+                    RuleRes::new(a,r)
+                } else {
+                    RuleRes::r(g)
+                },
+                _ => panic!()
+            },
+            Always => RuleRes::a(g)
+        }
+    }
 }
 
 
@@ -67,7 +234,7 @@ fn main(){
     let reg_att = Regex::new(r"([xmas])=(\d+)").unwrap();
     
     let mut rules = HashMap::new();
-    let mut gears = vec![];
+    //let mut gears = vec![];
     
     for l in text.iter() {
         if let Some(cap) = reg_rule.captures(l) {
@@ -81,7 +248,7 @@ fn main(){
                 };
                 rules.get_mut(&name).unwrap().push((r,c[4].to_string()));
             }
-        }
+        }/*
         else if let Some(cap) = reg_gear.captures(l) {
             let mut g = Gear{x:0,m:0,a:0,s:0};
             for c in reg_att.captures_iter(&cap[1]) {
@@ -95,10 +262,10 @@ fn main(){
                 }
             }
             gears.push(g);
-        }
+        }*/
     }
     
-    
+    /*
     for g in gears.into_iter() {
         let mut name = "in".to_string();
         'l:loop {
@@ -115,6 +282,36 @@ fn main(){
                 break
             }
         }}
+    }*/
+    
+    
+    let mut list = vec![(GearRange::new((1,4000)),"in".to_string())];
+    let mut acc = vec![];
+    while let Some((g,n)) = list.pop() {
+        match n.as_str() {
+            "A" => acc.push(g),
+            "R" => continue,
+            _ => {
+            let mut g = g;
+            for (r,s) in rules[&n].iter() {
+                let res = r.test_range(g);
+                if let Some(a) = res.accepted {
+                    list.push((a,s.clone()))
+                }
+                if let Some(r) = res.rejected {
+                    g = r
+                } else {
+                    break
+                }
+            }}
+        }
     }
+    
+    
+    for g in acc.iter() {
+        //println!("{} {:?}",g.count(),g);
+        total+= g.count();
+    }
+    
     println!("{}", total)
 }
