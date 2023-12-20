1fdc0cc4d3a978ef9366655ae12fc9f387553b98
Mael Saillot <mael.saillot@trlecomnancy.eu>
2023-12-20 13:09:56 +0100

    D20P1

--- main.rs
+++ main.rs
@@ -1,5 +1,5 @@
 use std::fs::read_to_string;
-use std::collections::HashMap;
+use std::collections::{BTreeMap,VecDeque};
 use regex::Regex;
 
 fn read_input() -> Vec<String> {
@@ -10,308 +10,117 @@ fn read_input() -> Vec<String> {
        .collect()
 }
 
-
-#[derive(Clone,Copy,Debug)]
-struct Gear {
-    x: u32,
-    m: u32,
-    a: u32,
-    s: u32
+#[derive(Clone,Copy,Debug,Eq,PartialEq)]
+enum Signal {
+    Low,
+    High
 }
-
-impl Gear {
-    fn sum(&self) -> u32 {
-        self.x+self.m+self.a+self.s
-    }
-}
-
-
-
-#[derive(Clone,Copy,Debug)]
-struct GearRange {
-    x: (u32,u32),
-    m: (u32,u32),
-    a: (u32,u32),
-    s: (u32,u32)
-}
-
-impl GearRange {
-    fn new(t:(u32,u32)) -> GearRange {
-        GearRange {
-            x:t,
-            m:t,
-            a:t,
-            s:t
-        }
-    }
-
-    fn count(&self) -> u64 {
-        fn len(t:(u32,u32)) -> u64 {
-            (t.1-t.0+1) as u64
-        }
-        len(self.x)*len(self.m)*len(self.a)*len(self.s)
-    }
-    
-    fn split_x(&self,l:u32) -> (GearRange,GearRange) {
-        let mut inf = self.clone();
-        let mut sup = self.clone();
-        inf.x.1 = l;
-        sup.x.0 = l+1;
-        (inf,sup)
-    }
-    
-    fn split_m(&self,l:u32) -> (GearRange,GearRange) {
-        let mut inf = self.clone();
-        let mut sup = self.clone();
-        inf.m.1 = l;
-        sup.m.0 = l+1;
-        (inf,sup)
-    }
-    
-    fn split_a(&self,l:u32) -> (GearRange,GearRange) {
-        let mut inf = self.clone();
-        let mut sup = self.clone();
-        inf.a.1 = l;
-        sup.a.0 = l+1;
-        (inf,sup)
-    }
-    
-    fn split_s(&self,l:u32) -> (GearRange,GearRange) {
-        let mut inf = self.clone();
-        let mut sup = self.clone();
-        inf.s.1 = l;
-        sup.s.0 = l+1;
-        (inf,sup)
-    }
-}
+use Signal::{Low,High};
 
 
 #[derive(Clone,Debug)]
-enum Rule {
-    Sup(String,u32),
-    Inf(String,u32),
-    Always
+enum Module {
+    Broadcast,
+    FlipFlop(bool),
+    Conj(BTreeMap<String,Signal>),
+    Blank
 }
-use Rule::{Sup,Inf,Always};
-
-struct RuleRes<T> {
-    accepted: Option<T>,
-    rejected: Option<T>
-}
+use Module::{Broadcast,FlipFlop,Conj,Blank};
 
 
-impl<T> RuleRes<T> {
-    fn new(a:T,r:T) -> RuleRes<T>{
-        RuleRes {
-            accepted: Some(a),
-            rejected: Some(r)
-        }
-    }
-
-    fn a(v:T) -> RuleRes<T> {
-        RuleRes {
-            accepted: Some(v),
-            rejected: None
-        }
+impl Module {
+    fn add_prec(&mut self, s: String) {
+        let Conj(h) = self else {return};
+        h.insert(s,Low);
     }
     
-    fn r(v:T) -> RuleRes<T> {
-        RuleRes {
-            accepted: None,
-            rejected: Some(v)
-        }
-    }
-}
-
-
-impl Rule {
-    fn test(&self,g:Gear) -> bool {
+    fn receive(&mut self, s: Signal, sender: String) -> Option<Signal> {
         match self {
-            Sup(s,v) => match s.as_str() {
-                "x" => g.x>*v,
-                "m" => g.m>*v,
-                "a" => g.a>*v,
-                "s" => g.s>*v,
-                _ => panic!()
+            Blank => None,
+            Broadcast => Some(s),
+            FlipFlop(state) => match s {
+                High => None,
+                Low => {
+                    *state = !*state;
+                    if *state {
+                        Some(High)
+                    } else {
+                        Some(Low)
+                    }
+                }
             },
-            Inf(s,v) => match s.as_str() {
-                "x" => g.x<*v,
-                "m" => g.m<*v,
-                "a" => g.a<*v,
-                "s" => g.s<*v,
-                _ => panic!()
-            },
-            Always => true
-        }
-    }
-    
-    
-    fn test_range(&self,g:GearRange) -> RuleRes<GearRange> {
-        match self {
-            Sup(s,v) => match s.as_str() {
-                "x" => if g.x.0>*v {
-                    RuleRes::a(g)
-                } else if g.x.1 >*v {
-                    let (r,a) = g.split_x(*v);
-                    RuleRes::new(a,r)
+            Conj(state) => {
+                state.insert(sender,s);
+                if state.iter().all(|(_,s)| *s==High) {
+                    Some(Low)
                 } else {
-                    RuleRes::r(g)
-                },
-                "m" => if g.m.0>*v {
-                    RuleRes::a(g)
-                } else if g.m.1 >*v {
-                    let (r,a) = g.split_m(*v);
-                    RuleRes::new(a,r)
-                } else {
-                    RuleRes::r(g)
-                },
-                "a" => if g.a.0>*v {
-                    RuleRes::a(g)
-                } else if g.a.1 >*v {
-                    let (r,a) = g.split_a(*v);
-                    RuleRes::new(a,r)
-                } else {
-                    RuleRes::r(g)
-                },
-                "s" => if g.s.0>*v {
-                    RuleRes::a(g)
-                } else if g.s.1 >*v {
-                    let (r,a) = g.split_s(*v);
-                    RuleRes::new(a,r)
-                } else {
-                    RuleRes::r(g)
-                },
-                _ => panic!()
-            },
-            Inf(s,v) => match s.as_str() {
-                "x" => if g.x.1<*v {
-                    RuleRes::a(g)
-                } else if g.x.0 <*v {
-                    let (a,r) = g.split_x(*v-1);
-                    RuleRes::new(a,r)
-                } else {
-                    RuleRes::r(g)
-                },
-                "m" => if g.m.1<*v {
-                    RuleRes::a(g)
-                } else if g.m.0 <*v {
-                    let (a,r) = g.split_m(*v-1);
-                    RuleRes::new(a,r)
-                } else {
-                    RuleRes::r(g)
-                },
-                "a" => if g.a.1<*v {
-                    RuleRes::a(g)
-                } else if g.a.0 <*v {
-                    let (a,r) = g.split_a(*v-1);
-                    RuleRes::new(a,r)
-                } else {
-                    RuleRes::r(g)
-                },
-                "s" => if g.s.1<*v {
-                    RuleRes::a(g)
-                } else if g.s.0 <*v {
-                    let (a,r) = g.split_s(*v-1);
-                    RuleRes::new(a,r)
-                } else {
-                    RuleRes::r(g)
-                },
-                _ => panic!()
-            },
-            Always => RuleRes::a(g)
+                    Some(High)
+                }
+            }
         }
     }
 }
 
 
 fn main(){
-    let mut total = 0;
+    //let mut total = 0;
     let text = read_input();
-    let reg_rule = Regex::new(r"^(\w+)\{(.+)\}$").unwrap();
-    let reg_act = Regex::new(r"(?:(\w+)([<>])(\d+):)?(\w+)").unwrap();
+    let reg = Regex::new(r"^([%&]?)(\w+) -> (.+)$").unwrap();
     
-    let reg_gear = Regex::new(r"^\{(.+)\}$").unwrap();
-    let reg_att = Regex::new(r"([xmas])=(\d+)").unwrap();
-    
-    let mut rules = HashMap::new();
-    //let mut gears = vec![];
-    
-    for l in text.iter() {
-        if let Some(cap) = reg_rule.captures(l) {
-            let name = cap[1].to_string();
-            rules.insert(cap[1].to_string(),vec![]);
-            for c in reg_act.captures_iter(&cap[2]) {
-                let r = match c.get(2).and_then(|m| Some(m.as_str())) {
-                    Some("<") => Inf(c[1].to_string(),c[3].parse::<u32>().unwrap()),
-                    Some(">") => Sup(c[1].to_string(),c[3].parse::<u32>().unwrap()),
-                    _ => Always
-                };
-                rules.get_mut(&name).unwrap().push((r,c[4].to_string()));
+    let mut mods = BTreeMap::new();
+    let mut temp = vec![];
+    for line in text.iter() {
+        let cap = reg.captures(line).unwrap();
+        let name = &cap[2];
+        temp.push((name.to_string(),cap[3].to_string()));
+        mods.insert(name.to_string(), match name {
+            "broadcaster" => Broadcast,
+            _ => match &cap[1] {
+                "%" => FlipFlop(false),
+                "&" => Conj(BTreeMap::new()),
+                _ => panic!()
             }
-        }/*
-        else if let Some(cap) = reg_gear.captures(l) {
-            let mut g = Gear{x:0,m:0,a:0,s:0};
-            for c in reg_att.captures_iter(&cap[1]) {
-                let v = c[2].parse::<u32>().unwrap();
-                match &c[1] {
-                    "x" => g.x = v,
-                    "m" => g.m = v,
-                    "a" => g.a = v,
-                    "s" => g.s = v,
-                    _ => panic!()
-                }
-            }
-            gears.push(g);
-        }*/
+        });
     }
     
-    /*
-    for g in gears.into_iter() {
-        let mut name = "in".to_string();
-        'l:loop {
-        for (r,s) in rules[&name].iter() {
-            if r.test(g) {
-                match s.as_str() {
-                    "A" => {
-                        total+=g.sum();
-                        break 'l
-                    },
-                    "R" => break 'l,
-                    _ => name = s.clone()
-                }
-                break
+    let mut links = BTreeMap::new();
+    for (name,suivs) in temp.into_iter() {
+        let mut l = vec![];
+        for n in suivs.split(", ") {
+            if !mods.contains_key(n) {
+                mods.insert(n.to_string(),Blank);
             }
-        }}
-    }*/
-    
-    
-    let mut list = vec![(GearRange::new((1,4000)),"in".to_string())];
-    let mut acc = vec![];
-    while let Some((g,n)) = list.pop() {
-        match n.as_str() {
-            "A" => acc.push(g),
-            "R" => continue,
-            _ => {
-            let mut g = g;
-            for (r,s) in rules[&n].iter() {
-                let res = r.test_range(g);
-                if let Some(a) = res.accepted {
-                    list.push((a,s.clone()))
-                }
-                if let Some(r) = res.rejected {
-                    g = r
-                } else {
-                    break
-                }
-            }}
+            l.push(n.to_string());
+            mods.get_mut(n).unwrap().add_prec(name.clone());
         }
+        links.insert(name,l);
+    }
+    
+    let links = links;
+    let mut htot = 0;
+    let mut ltot = 0;
+    
+    for _ in 0..1000 {
+        let mut process = VecDeque::from([("button".to_string(),Low,"broadcaster".to_string())]);
+        let mut lcount= 0;
+        let mut hcount =0;
+        
+        while let Some((em,sig,rec)) = process.pop_front() {
+            //println!("{} -{:?}-> {}", em,sig,rec);
+            match sig {
+                Low => lcount+=1,
+                High => hcount += 1
+            }
+            let Some(sig) = mods.get_mut(&rec).unwrap().receive(sig,em) else {continue};
+            let em = rec;
+            for rec in links[&em].iter() {
+                process.push_back((em.clone(),sig,rec.clone()))
+            }
+        }
+        ltot+=lcount;
+        htot+=hcount;
     }
     
     
-    for g in acc.iter() {
-        //println!("{} {:?}",g.count(),g);
-        total+= g.count();
-    }
-    
-    println!("{}", total)
+    println!("{}", htot*ltot)
 }
