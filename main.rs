4668272f5f33e554ebb25943b6b08f23e5333ebd
Mael Saillot <mael.saillot@trlecomnancy.eu>
2023-12-25 12:20:16 +0100

    D23P2

--- main.rs
+++ main.rs
@@ -1,5 +1,5 @@
 use std::fs::read_to_string;
-//use std::collections::BTreeSet;
+use std::collections::{BTreeSet,HashMap};
 //use regex::Regex;
 
 fn read_input() -> Vec<String> {
@@ -41,28 +41,50 @@ impl Dir {
 #[derive(Clone,Copy,Debug)]
 enum Cell{
     Blank,
-    Wall,
-    Slop(Dir)
+    Wall
 }
-use Cell::{Blank,Wall,Slop};
+use Cell::{Blank,Wall};
 
 
-fn path(g: &Vec<Vec<Cell>>, m: &mut Vec<Vec<bool>>, (i,j): (usize,usize)) -> Option<usize> {
-    if i < g.len() && j < g[0].len() && !m[i][j] {
-        m[i][j]=true;
-        let rep = match g[i][j] {
-            Wall => None,
-            Slop(d) => d.depl((i,j)).and_then(|p| path(g,m,p)),
-            Blank => if i == g.len()-1 {
-                Some(0)
-            } else {
-                [Up,Down,Right,Left].into_iter().filter_map(|d| d.depl((i,j)).and_then(|p| path(g,m,p))).max()
-            }
-        }.and_then(|x| Some(x+1));
-        m[i][j] = false;
-        return rep;
+fn is_free(g: &Vec<Vec<Cell>>, (i,j): (usize,usize)) -> bool{
+    i < g.len() && j < g[0].len() && match g[i][j] {
+        Blank => true,
+        Wall => false
     }
-    None
+}
+
+
+fn nexts(g: &Vec<Vec<Cell>>, (i,j): (usize,usize)) -> impl Iterator<Item=(usize,usize)>+'_ {
+     [Up,Down,Right,Left].into_iter().filter_map(move |d| d.depl((i,j))).filter(|p| is_free(g,*p))
+}
+
+
+fn parcours(g: &Vec<Vec<Cell>>, m: &BTreeSet<(usize,usize)>, from: (usize,usize), p: (usize,usize), len: u32) -> ((usize,usize),u32) {
+    if m.contains(&p) {
+        return (p,len)
+    }
+    let mut v = nexts(g,p);
+    let Some(mut to) = v.next() else {
+        panic!("Pas de voisins")
+    };
+    while to == from {
+        to = v.next().unwrap();
+    }
+    return parcours(g,m,p,to,len+1)
+}
+
+
+fn f(g: &HashMap<(usize,usize),Vec<((usize,usize),u32)>>, m: &mut BTreeSet<(usize,usize)>, p: (usize,usize), len: u32, end: (usize,usize)) -> Option<u32> {
+    if m.contains(&p) {
+        None
+    } else if p==end {
+        Some(len)
+    } else {
+        m.insert(p);
+        let rep= g[&p].iter().filter_map(|(p,l)| f(g,m,*p,len+*l,end)).max();
+        m.remove(&p);
+        rep
+    }
 }
 
 
@@ -72,17 +94,41 @@ fn main(){
     //let reg = Regex::new(r"^(\d+),(\d+),(\d+)~(\d+),(\d+),(\d+)$").unwrap();
     
     let grid: Vec<Vec<Cell>> = text.into_iter().map(|s| s.chars().map(|c| match c {
-        '.' => Blank,
         '#' => Wall,
-        '>' => Slop(Right),
-        '<' => Slop(Left),
-        '^' => Slop(Up),
-        'v' => Slop(Down),
-        _ => panic!()
+        _ => Blank
     }).collect()).collect();
-    let mut mark = vec![vec![false;grid[0].len()];grid.len()];
     
-    let len = (0..grid[0].len()).filter_map(|j| path(&grid, &mut mark, (0,j))).next().unwrap()-1;
+    let j = (0..grid[0].len()).filter(|j| match grid[0][*j] {Blank=> true, Wall=> false}).next().unwrap();
+    let start = (0,j);
+    let j = (0..grid[0].len()).filter(|j| match grid[grid.len()-1][*j] {Blank=> true, Wall=> false}).next().unwrap();
+    let end = (grid.len()-1,j);
     
-    println!("{}", len)
+    
+    let mut nodes = BTreeSet::new();
+    nodes.insert(start);
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            if is_free(&grid,(i,j)) && nexts(&grid,(i,j)).count() > 2 {
+                nodes.insert((i,j));
+                
+            }
+        }
+    }
+    nodes.insert(end);
+    
+    let mut graph = HashMap::new();
+    for p in nodes.clone().into_iter() {
+        let mut voisins = vec![];
+        for v in nexts(&grid,p) {
+            voisins.push(parcours(&grid,&nodes,p,v,1))
+        }
+        graph.insert(p,voisins);
+    }
+     ;
+    
+    let mut mark=BTreeSet::new();
+    let total = f(&graph,&mut mark, start, 0,end).unwrap();
+    
+    println!("{}",total)
 }
+6682
